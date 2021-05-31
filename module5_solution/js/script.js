fc805fbc2a009836351a08666de8cfb5d7d4af3c
Rahul Kanna <67679039+RahulKanna05@users.noreply.github.com>
2021-05-31 19:33:41 +0530

    complete solution of module-5

--- module5_solution/js/script.js
+++ module5_solution/js/script.js
@@ -40,10 +40,8 @@ var showLoading = function (selector) {
 // with propValue in given 'string'
 var insertProperty = function (string, propName, propValue) {
   var propToReplace = "{{" + propName + "}}";
-  
   string = string
     .replace(new RegExp(propToReplace, "g"), propValue);
-
   return string;
 };
 
@@ -117,7 +115,7 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,"randomCategoryShortName","'"+chosenCategoryShortName+"'");
+      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml,'randomCategoryShortName',"'"+chosenCategoryShortName+"'");
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
